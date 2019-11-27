f19642b41fe4eb31763f35bdc88bbb5a6d259793
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-11-27 14:01:24 -0500

    inputs

--- avrpin.h
+++ avrpin.h
@@ -2,10 +2,13 @@
 #define __AVRPIN_H
 
 
+#include <avr/io.h>
+
 #define BIT(n) (1 << n)
 #define _PIN(port, pinnum) P ## port ## pinnum
 #define _DDR(port) DDR ## port
 #define _PORT(port) PORT ## port
+#define _INPUT(port) PIN ## port
 #define PINDEF(port, pinnum) port, BIT(_PIN(port, pinnum))
 
 #define _SET_BIT_LOW(reg, bit) do { reg &= ~bit; } while(0)
@@ -18,6 +21,9 @@
 #define WRITE_OUT(pindef, state) _WRITE_OUT_ ## state(pindef)
 #define _WRITE_OUT_HIGH(port, pinbit) _SET_BIT_HIGH(_PORT(port), pinbit)
 #define _WRITE_OUT_LOW(port, pinbit) _SET_BIT_LOW(_PORT(port), pinbit)
+
+#define READ_IN(pindef) _READ_IN(pindef)
+#define _READ_IN(port, pinbit) (_INPUT(port) & pinbit)
 
 
 #endif
